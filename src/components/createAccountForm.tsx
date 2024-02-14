0ede539d0126864171c00a4044763994f8e7e2ba
Tyler Tedesco <s_tylert@icloud.com>
2024-02-14 12:34:20 -0700

    Add field inputs

--- src/components/createAccountForm.tsx
+++ src/components/createAccountForm.tsx
@@ -4,12 +4,24 @@ export const CreateAccountForm = () => {
       <h1>Create Account</h1>
       <form>
         <label>
-          Email
-          <input type="email" />
+          Name
+          <input type="name" />
         </label>
         <label>
-          Password
-          <input type="password" />
+          Location
+          <input type="location" />
+        </label>
+        <label>
+          Skills you would like to learn
+          <select />
+        </label>
+        <label>
+          Skills you would like to teach
+          <select />
+        </label>
+        <label>
+          About
+          <input type="summary" />
         </label>
         <button type="submit">Create Account</button>
       </form>
