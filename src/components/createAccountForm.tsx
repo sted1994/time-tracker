d76a2c4e8f86ad20119cec97a58de80773b638cb
Tyler Tedesco <s_tylert@icloud.com>
2024-02-15 13:34:08 -0700

    Convert form fields to MUI components

--- src/components/createAccountForm.tsx
+++ src/components/createAccountForm.tsx
@@ -1,30 +1,103 @@
+import {
+  Box,
+  Button,
+  Checkbox,
+  Container,
+  FormLabel,
+  ListItemText,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+  styled,
+} from "@mui/material";
+
 export const CreateAccountForm = () => {
+  const InputGroup = styled("div")({
+    display: "flex",
+    flexDirection: "column",
+    gap: 10,
+  });
+
+  const fillerSkills = [
+    "Carpentry",
+    "Cooking",
+    "Dancing",
+    "Drawing",
+    "Fishing",
+    "Gardening",
+    "Knitting",
+    "Painting",
+    "Photography",
+    "Pottery",
+    "Sewing",
+    "Singing",
+    "Woodworking",
+    "Writing",
+    "Yoga",
+    "Other",
+  ];
+
   return (
-    <div>
-      <h1>Create Account</h1>
-      <form>
-        <label>
-          Name
-          <input type="name" />
-        </label>
-        <label>
-          Location
-          <input type="location" />
-        </label>
-        <label>
-          Skills you would like to learn
-          <select />
-        </label>
-        <label>
-          Skills you would like to teach
-          <select />
-        </label>
-        <label>
+    <Container sx={{ backgroundColor: "#D9D9D9", borderRadius: 5 }}>
+      <Typography variant="h4">Create Account</Typography>
+      <Container component="form">
+        <Container sx={{ display: "flex" }}>
+          <InputGroup>
+            <FormLabel>
+              Name
+              <TextField
+                id="outlined-basic"
+                label="Enter Name"
+                variant="outlined"
+              />
+            </FormLabel>
+            <FormLabel>
+              Location
+              <TextField
+                id="outlined-basic"
+                label="Enter Location"
+                variant="outlined"
+              />
+            </FormLabel>
+          </InputGroup>
+          <InputGroup>
+            <FormLabel>
+              Skills you would like to learn
+              <Select multiple value={[""]}>
+                {fillerSkills.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    <Checkbox />
+                    <ListItemText primary={name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormLabel>
+            <FormLabel>
+              Skills you would like to teach
+              <Select multiple value={[""]}>
+                {fillerSkills.map((name) => (
+                  <MenuItem key={name} value={name}>
+                    <Checkbox />
+                    <ListItemText primary={name} />
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormLabel>
+          </InputGroup>
+        </Container>
+        <FormLabel>
           About
-          <input type="summary" />
-        </label>
-        <button type="submit">Create Account</button>
-      </form>
-    </div>
+          <TextField
+            id="outlined-basic"
+            label="Enter a short bio about yourself"
+            variant="outlined"
+          />
+        </FormLabel>
+        <Button variant="outlined" type="submit">
+          Submit
+        </Button>
+      </Container>
+    </Container>
   );
 };
